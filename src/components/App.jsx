d1bc926f1823fbb9bc03627a0e0c12123fae8349
RamirezAgus <rlagustin0@gmail.com>
2023-06-05 20:58:21 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+/**
+ * Funcion anonima para crear un Componente principal
+ * @returns {React.Component} Componente principal de nuestra aplicacion
+ */
+const App = () => {
+    return (
+        <div>
+            <h1>Proyecto final</h1>
+        </div>
+    );
+}
+
+export default App;
